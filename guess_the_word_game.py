64803eb70c96d5e14dc8e1530cd8ae641c2af501
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2025-01-27 22:22:05 +0800

    the underscore appears

--- guess_the_word_game.py
+++ guess_the_word_game.py
@@ -1,9 +1,27 @@
 import random 
 
-#words that can be guessed 
-words = ["apple", "banana", "programming","tiger", "lamp", "television", "projects", "water",
-         "cake", "flower", "car","clouds", "pencil", "dress", "watch", "cat", "paper"]
+# Words that can be guessed 
+words = ["apple", "banana", "programming", "tiger", "lamp", "television", "projects", "water",
+         "cake", "flower", "car", "clouds", "pencil", "dress", "watch", "cat", "paper"]
 
-random_words = random.choice(words)
+# Randomly choose a word
+random_word = random.choice(words)
 
 print("GUESS THE WORD".center(30, "*"))
+print("Hint: the word has", len(random_word), "letters.")
+
+user_guesses = ''
+chances = 5 
+
+while chances > 0: 
+    wrong_guesses = 0 
+
+    for character in random_word:
+        if character in user_guesses: 
+            print(f"Correct guess:{character}")  
+        else:
+            wrong_guesses += 1 
+            print("_")
+
+
+   
